97b475dae3df4b3db07d5d9ce8ce5bbfa1771e7e
Anand Chitipothu <anandology@gmail.com>
2014-06-17 01:22:39 +0530

    The Place model is more mature now with test cases.
    Using many-to-many mapping for handling parents of a place.

--- cleansweep/models.py
+++ cleansweep/models.py
@@ -3,31 +3,13 @@ from .app import app
 
 db = SQLAlchemy(app)
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(20), unique=True)
-    phone = db.Column(db.String(20), unique=True)
-    email = db.Column(db.String(120), unique=True)
-    place_id = db.Column(db.Integer, db.ForeignKey('place.id'))
-    place = db.relationship('Place',
-        backref=db.backref('members', lazy='dynamic'))
-
-    def __init__(self, name, phone, email, place=None):
-        self.name = name
-        self.phone = phone
-        self.email = email
-        self.place = place
-
-    def __repr__(self):
-        return '<User %r>' % self.username
-
 class PlaceType(db.Model):
     """There are different types of places in the hierarchy like
     country, state, region etc. This table captures that.
     """
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.Text, nullable=False)
-    short_name = db.Column(db.Text)
+    short_name = db.Column(db.Text, nullable=False, unique=True)
 
     # number to indicate level of the type. For example:
     # 10 for country
@@ -41,15 +23,81 @@ class PlaceType(db.Model):
         self.short_name = short_name
         self.level = level
 
+    def __repr__(self):
+        return '<%s>' % self.short_name
+
+    @staticmethod
+    def get(short_name):
+        """Returns PlaceType object with given short_name.
+        """
+        return PlaceType.query.filter_by(short_name=short_name).first()
+
+    @staticmethod
+    def new(name, short_name, level):
+        t = PlaceType(name, short_name, level)
+        db.sesson.add(t)
+
+place_parents = db.Table('place_parents',
+    db.Column('parent_id', db.Integer, db.ForeignKey('place.id')),
+    db.Column('child_id', db.Integer, db.ForeignKey('place.id'))
+)
+
 class Place(db.Model):
+    __table_name__ = "place"
     id = db.Column(db.Integer, primary_key=True)
-    code = db.Column(db.Text, nullable=False)
+    key = db.Column(db.Text, nullable=False)
     name = db.Column(db.Text, nullable=False)
     type_id = db.Column(db.Integer, db.ForeignKey('place_type.id'))
+    type = db.relationship('PlaceType',
+        backref=db.backref('places', lazy='dynamic'))
 
-    def add_member(self, member):
-        member.place = self
-        db.session.add(member)
-        db.session.add(self)
+    # List of parents
+    parents = db.relationship('Place', 
+        secondary=place_parents, 
+        primaryjoin=(id==place_parents.c.child_id),
+        secondaryjoin=(id==place_parents.c.parent_id),
+        backref=db.backref('places', lazy='dynamic', order_by='Place.key'))
 
+    def __init__(self, key, name, type):
+        self.key = key
+        self.name = name
+        self.type = type
+
+    def __repr__(self):
+        return "Place(%r)" % self.key
+
+    @staticmethod
+    def find(key):
+        return Place.query.filter_by(key=key).first()
+
+    def get_parent(self, type):
+        """Returns parent place of given type.
+        """
+        try:
+            return [p for p in self.parents if p.type == type][0]
+        except IndexError:
+            return None
+
+    def get_places(self, type=None):
+        """Returns all places of given type below this place.
+        """
+        return self._get_places_query(type=type).all()
+
+    def get_places_count(self, type=None):
+        return self._get_places_query(type=type).count()
+
+    def _get_places_query(self, type=None):
+        q = self.places;
+        if type:
+            q = q.join(PlaceType).filter(PlaceType.id==type.id)
+        return q
+
+    def add_place(self, place):
+        """Addes a new place as direct child of this place.
+
+        This function takes care of setting parents for the 
+        new place.
+        """
+        place.parents = self.parents + [self]
+        db.session.add(place)
 
---
+++ cleansweep/tests/test_models.py
@@ -0,0 +1,76 @@
+from flask.ext.testing import TestCase
+
+from ..app import app
+from ..models import db, Place, PlaceType
+
+
+class DBTestCase(TestCase):
+    def create_app(self):     
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'   
+        return app
+
+    def setUp(self):
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+
+class PlaceTypeTest(DBTestCase):
+    def testPlaceType(self):
+        t1 = PlaceType("State", "STATE", 20)
+        db.session.add(t1)
+        db.session.commit()
+
+        t = PlaceType.get("STATE")
+        self.assertEquals(t.name, 'State')
+        self.assertEquals(t.level, 20)
+
+class PlaceTest(DBTestCase):
+    def setUp(self):
+        DBTestCase.setUp(self)
+
+        def add(name, short_name, level):
+            t = PlaceType(name, short_name, level)
+            db.session.add(t)
+            return t
+
+        self.STATE = add('State', 'STATE', 10)
+        self.REGION = add('Region', 'REGION', 20)
+        self.LC = add('Loksabha Constituency', 'LC', 30)
+        db.session.commit()
+
+    def test_create(self):
+        p = Place('KA', 'Karnataka', self.STATE)
+        db.session.add(p)
+        db.session.commit()
+
+        ka = Place.find('KA')
+        self.assertTrue(ka is not None)
+        self.assertEquals(ka.key, 'KA')
+        self.assertEquals(ka.name, 'Karnataka')
+        self.assertEquals(ka.type.name, 'State')
+        self.assertEquals(ka.parents, [])
+
+    def test_add_place(self):
+        KA = Place('KA', 'Karnataka', self.STATE)
+        db.session.add(KA)
+        db.session.commit()
+
+        R01 = Place('KA/R01', 'Bangalore Region', self.REGION)
+        KA.add_place(R01)
+
+        LC24 = Place('KA/LC24', 'Bangalore North', self.LC)
+        KA.add_place(LC24)
+
+        LC26 = Place('KA/LC26', 'Bangalore South', self.LC)
+        KA.add_place(LC26)
+        db.session.commit()
+
+        self.assertEquals(KA.get_places(), [LC24, LC26, R01])
+        self.assertEquals(KA.get_places(type=self.REGION), [R01])
+        self.assertEquals(KA.get_places(type=self.LC), [LC24, LC26])
+
+if __name__ == '__main__':
+    import unittest
+    unittest.main()
