9279b5fd00c5ea12a658b212ef80ae872d6081ac
Anand Chitipothu <anandology@gmail.com>
2014-06-17 14:27:00 +0530

    Added view for displaying places.
    Places are going to be main component of this website, so added a new
    converter for matching place keys.

--- cleansweep/views/place.py
+++ cleansweep/views/place.py
@@ -1,6 +1,36 @@
+from flask import render_template, abort
+from werkzeug.routing import BaseConverter
 from ..app import app
-from flask import render_template
+from ..models import Place
+
+class PlaceConverter(BaseConverter):
+    """Converter for place.
+
+    Places will have URLs like KA, KA/LC12, KA/AC123, KA/AC123/PB0123 etc.
+    We'll also have URLS for actions on places as KA/LC12/members etc. This
+    converter provide support for place converter, so that we can use routes
+    like:
+
+        @app.route(/<place:key>)
+        ...
+
+        @app.route(/<place:key>/members)
+        ...
+    """
+    def __init__(self, url_map, *items):
+        super(PlaceConverter, self).__init__(url_map)
+        self.regex = '[A-Z0-9/]+'
+
+app.url_map.converters['place'] = PlaceConverter
 
 @app.route("/")
 def home():
     return render_template("home.html")
+
+@app.route("/<place:key>")
+def place(key):
+    place = Place.find(key)
+    if not place:
+        abort(404)
+    else:
+        return render_template("place.html", place=place)
