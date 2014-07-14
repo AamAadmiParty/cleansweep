edda416ef572e4591e8ec0d2db07b60c64007607
Anand Chitipothu <anandology@gmail.com>
2014-07-14 15:48:58 +0530

    oops! forgot to add forms.py #12

---
+++ cleansweep/forms.py
@@ -0,0 +1,11 @@
+from flask_wtf import Form
+from wtforms import StringField
+from wtforms import validators
+
+class AddMemberForm(Form):
+    name = StringField('Name', [validators.Required()])
+    phone = StringField('Phone Number', [
+        validators.Required(), 
+        validators.Regexp(r'\d{10}', message="Please enter 10 digit phone number")])
+    email = StringField('Email Address', [validators.Email()])
+    voterid = StringField('Voter ID')
