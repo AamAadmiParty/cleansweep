3989d37b2e4d1b59587d7d3013cfacbe5065007e
Anand Chitipothu <anandology@gmail.com>
2014-10-25 16:25:16 +0530

    Send welcome email to volunteer on signup.

--- cleansweep/core/mailer.py
+++ cleansweep/core/mailer.py
@@ -2,17 +2,50 @@ from ..app import app
 from envelopes import Envelope
 from rq import Queue
 from redis import Redis
+import pynliner
 
-def sendmail(to_address, subject, message, reply_to=None, cc=None, bcc=None):
+class Message:
+    def __init__(self, to_addr=None, subject=None, reply_to=None):
+        self.to_addr = to_addr
+        self.subject = subject
+        self.reply_to = None
+        self.text_body = None
+        self.html_body = None
+
+    def set_subject(self, subject):
+        self.subject = subject
+        return ""
+
+    def send(self):
+        sendmail(
+            to_address=self.to_addr,
+            subject=self.subject,
+            message=self.text_body,
+            message_html=self.html_body,
+            reply_to=self.reply_to)
+
+    def send_async(self):
+        sendmail_async(
+            to_address=self.to_addr,
+            subject=self.subject,
+            message=self.text_body,
+            message_html=self.html_body,
+            reply_to=self.reply_to)
+
+def sendmail(to_address, subject, message, message_html=None, reply_to=None, cc=None, bcc=None):
     headers = {}
     if reply_to:
         headers['Reply-To'] = reply_to
+
+    if message_html:
+        message_html = pynliner.fromString(message_html)
 
     envelope = Envelope(
         from_addr=app.config['FROM_ADDRESS'],
         to_addr=to_address,
         subject=subject,
         text_body=message,
+        html_body=message_html,
         headers=headers,
         cc_addr=cc,
         bcc_addr=bcc
@@ -45,7 +78,6 @@ def run_worker():
     from rq import Worker, Connection
     with Connection():
         q = get_queue()
-        print q
         qs = [q]
         w = Worker(qs)
         w.work()
--- cleansweep/notifications.py
+++ cleansweep/notifications.py
@@ -1,5 +1,6 @@
-from .core import signals
+from .core import signals, mailer
 from .models import db
+from flask import render_template
 
 @signals.login_successful.connect
 def on_login_update_ids(user, userdata):
@@ -11,3 +12,12 @@ def on_login_update_ids(user, userdata):
 	if service == 'facebook':
 		user.add_details('facebook_id', userdata['facebook_id'])
 		db.session.commit()
+
+@signals.volunteer_signup.connect
+def on_volunteer_signup(volunteer):
+	if not volunteer.email:
+		return
+
+	message = mailer.Message(to_addr=volunteer.email)
+	message.html_body = render_template("emails/volunteer_signup.html", volunteer=volunteer, message=message)
+	message.send()
