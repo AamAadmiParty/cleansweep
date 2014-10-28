341c9901ce389c32867f6a709771ce4c55717ebe
Anand Chitipothu <anandology@gmail.com>
2014-10-28 21:06:19 +0530

    removed obsolete code.
    It was already moved to signups/views.py

--- cleansweep/views/account.py
+++ cleansweep/views/account.py
@@ -7,7 +7,7 @@ from ..app import app
 from ..models import Member, PendingMember
 from .. import oauth
 from .. import forms
-from ..core import signups, signals
+from ..core import signals
 
 @app.route("/account/login")
 def login():
@@ -28,41 +28,6 @@ def login():
 def logout():
     session.clear()
     return redirect(url_for("home"))
-
-@app.route("/account/signup", methods=["GET", "POST"])
-def signup():
-    userdata = session.get("oauth")
-
-    # is user autheticated?
-    if not userdata:
-        return render_template("signup.html", userdata=None)
-
-    # is already a member?
-    user = Member.find(email=userdata['email'])
-    if user:
-        session['user'] = user.email
-        return redirect(url_for("dashboard"))
-
-    # is already a member?
-    pending_member = PendingMember.find(email=userdata['email'])
-    if pending_member:
-        return render_template("signup.html", userdata=None, pending_member=pending_member)
-
-    # show the form
-    form = forms.SignupForm()
-    if request.method == "GET":
-        form.name.data = userdata['name']
-    if request.method == "POST" and form.validate():
-        person = signups.signup(
-            name=form.name.data,
-            email=userdata['email'],
-            phone=form.phone.data,
-            voterid=form.voterid.data,
-            place_key=form.place.data)
-        signals.volunteer_signup.send(person)
-        return render_template("signup_complete.html", person=person)
-    return render_template("signup.html", userdata=userdata, form=form)
-
 
 def get_host():
     # facebook doesn't seem to like 127.0.0.1
