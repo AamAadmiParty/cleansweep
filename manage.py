f44c61d19e53816dcd3c92ac8a83fdc03e22f4a8
Anand Chitipothu <anandology@gmail.com>
2015-09-24 09:43:06 +0530

    Tweaks to manage.py.
    * Changed the command names from add_member to add-member, runworker to run-worker etc.
    * Added help command
    
    Issue #109

--- manage.py
+++ manage.py
@@ -1,4 +1,4 @@
-from flask.ext.script import Manager
+from flask.ext.script import Manager, Command
 from flask.ext.migrate import Migrate, MigrateCommand
 
 from cleansweep.main import app
@@ -6,7 +6,22 @@ from cleansweep.models import db
 
 migrate = Migrate(app, db)
 
-manager = Manager(app)
+
+class CleansweepManager(Manager):
+    """Flask Script Manager with some customizations.
+    """
+    def command(self, func=None, name=None):
+        """Variant of @manager.command decorator provided by flask-script.
+
+        Adds `do_something` function as `do-something` command. It is more convenient
+        to type a hypen on command-line than an underscre.
+        """
+        command = Command(func)
+        name = func.__name__.replace("_", "-")
+        self.add_command(name, command)
+        return func
+
+manager = CleansweepManager(app)
 manager.add_command('db', MigrateCommand)
 
 
@@ -27,7 +42,7 @@ def load(directory):
 
 
 @manager.command
-def loadfile(filename):
+def load_file(filename):
     "Loads data from the specified file."
 
     from cleansweep.loaddata import main_loadfiles
@@ -43,12 +58,23 @@ def add_member(place, name, email, phone):
 
 
 @manager.command
-def runworker():
+def run_worker():
     "Runs a worker."
-
     from cleansweep.core.mailer import run_worker
     run_worker()
 
+@manager.command
+def help():
+    """Shows this help message.
+    """
+    print "USAGE: python manage.py command [arguments]"
+    print ""
+    print "The available commands are:"
+    print ""
+    width = max(len(name) for name in manager._commands)
+    for name, command in sorted(manager._commands.items()):
+        doc = getattr(command, 'help', None) or command.__doc__ or ""
+        print "    {:{w}s}\t{}".format(name, doc.strip(), w=width)
 
 if __name__ == '__main__':
     manager.run()
