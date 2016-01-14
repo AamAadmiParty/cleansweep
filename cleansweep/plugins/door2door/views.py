90137db1ec44f3c2c840d25be01a09a8040ca38d
Raghav Sharma <raghav_ftw@hotmail.com>
2016-01-15 05:04:05 +0530

    Added a delete button and a function in view to handle deletion of an entry. Also defined a permission to delete.

--- cleansweep/plugins/door2door/views.py
+++ cleansweep/plugins/door2door/views.py
@@ -1,5 +1,5 @@
 from cleansweep import forms
-from cleansweep.models import Place, db
+from cleansweep.models import Place, db, Door2DoorEntry
 from cleansweep.plugin import Plugin
 from cleansweep.view_helpers import require_permission
 from flask import render_template, request, redirect, url_for
@@ -12,12 +12,15 @@ plugin.define_permission(
     description='Permission to view door to door entries'
 )
 
-
 plugin.define_permission(
     name='door2door.add',
     description='Permission to make door to door entries'
 )
 
+plugin.define_permission(
+    name='door2door.delete',
+    description='Permission to delete door to door entries'
+)
 
 def init_app(app):
     plugin.init_app(app)
@@ -42,3 +45,13 @@ def make_entry(place):
         db.session.commit()
         return redirect(url_for(".door2door", place=place))
     return render_template("entry_door2door.html", place=place, form=form)
+
+
+@plugin.route("/<place:place>/door2door/delete/<_id>")
+@require_permission("door2door.delete")
+def delete_entry(place, _id):
+    entry = Door2DoorEntry.find(id=_id)
+    db.session.delete(entry)
+    db.session.commit()
+    return render_template("door2door.html", place=place)
+
