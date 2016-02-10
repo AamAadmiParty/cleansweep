d084f4cbba8069262718f24c509ea76903e8e7bf
Anup Mishra <er-anup@users.noreply.github.com>
2016-02-10 13:09:39 +0530

    Removed json, urllib2
    Removed json, urllib2 library import
    Removed google profile image condition

--- cleansweep/notifications.py
+++ cleansweep/notifications.py
@@ -1,8 +1,7 @@
 from .core import signals, mailer
 from .models import db
 from flask import render_template
-import urllib2
-import json
+
 
 @signals.login_successful.connect
 def on_login_update_ids(user, userdata):
@@ -16,6 +15,5 @@ def on_login_update_ids(user, userdata):
 		db.session.commit()
 	elif service == 'google':
 		user.add_details('google_id', userdata['google_id'])
-		user.add_details('google_image_url', json.loads(urllib2.urlopen( "https://www.googleapis.com/plus/v1/people/"+userdata['google_id']+"?fields=image&key=AIzaSyCdWe-KtVS8Gd-yBv-1pJQyqbbo_EY1Rs4" ).read())['image']['url'])
 		db.session.commit()
 
