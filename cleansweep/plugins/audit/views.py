14c74e3c46bec093bef0bd6f62da6ccc8a630ed8
Anand Chitipothu <anandology@gmail.com>
2016-03-30 17:11:40 +0530

    Added pagination to audit records.
    issue #180

--- cleansweep/plugins/audit/views.py
+++ cleansweep/plugins/audit/views.py
@@ -1,6 +1,7 @@
 from . import models
 from . import audit
 from ...plugin import Plugin
+from ... import helpers as h
 from ...view_helpers import require_permission
 from flask import (request, render_template)
 
@@ -8,11 +9,20 @@ from flask import (request, render_template)
 plugin = Plugin('audit', __name__, template_folder='templates')
 
 def init_app(app):
-	plugin.init_app(app)
-	plugin.add_sidebar_entry("Audit Trial", endpoint="audit_trail", permission="audit")
+    plugin.init_app(app)
+    plugin.add_sidebar_entry("Audit Trial", endpoint="audit_trail", permission="audit")
 
 @plugin.route("/<place:place>/audit", methods=['GET'])
 @require_permission("audit")
 def audit_trail(place):
-	action = request.args.get("action")
-	return render_template("audit.html", place=place, action=action)
+    action = request.args.get("action")
+    page = h.safeint(request.args.get("page", 1), default=1, minvalue=1)
+    perpage = 100
+    offset = (page-1)*perpage
+    audit_records = place.get_audit_records(action=action, offset=offset, limit=perpage)
+    return render_template("audit.html",
+            place=place,
+            audit_records=audit_records,
+            action=action,
+            page=page,
+            perpage=perpage)
