192fa119e16cbf3bc601f993bae2a7572f087f70
Anand Chitipothu <anandology@gmail.com>
2016-08-26 15:41:18 +0530

    Optimized door2door stats.
    It was taking too long time. Using created_date column and some other hacks to make it faster.

--- cleansweep/plugins/door2door/stats.py
+++ cleansweep/plugins/door2door/stats.py
@@ -1,6 +1,6 @@
+import datetime
 from cleansweep.models import db
 from cleansweep.stats import Stats, register_stats
-
 
 @register_stats
 class Door2DoorStats(Stats):
@@ -11,19 +11,31 @@ class Door2DoorStats(Stats):
     cummulative = True
 
     def get_timeseries_data(self, place):
+        # Anand: Querying only for 90-day window to make the query faster.
+        # For some reason, pg planner seems to be using the index only when
+        # queried with date bounds. Otherwise it is doing seqscan.
+        end_date = datetime.date.today()
+        begin_date = end_date - datetime.timedelta(days=90)
         q = (
-            "SELECT created::date as date, count(*) as count" +
+            "SELECT created_date as date, count(*) as count" +
             " FROM door2door_entry, place_parents p" +
             " WHERE place_id=p.child_id" +
             "   AND p.parent_id=%s" +
+            "   AND created_date > %s" +
+            "   AND created_date <= %s"
             " GROUP BY 1" +
             " ORDER BY 1 desc")
-        result = db.engine.execute(q, [place.id])
+        result = db.engine.execute(q, [place.id, begin_date, end_date])
         return list(result)
 
     def get_total(self, place):
-        # Anand: Improve this
-        return sum(count for date, count in self.get_timeseries_data(place))
+        q = (
+            "SELECT count(*) as count FROM door2door_entry, place_parents p" +
+            " WHERE place_id=p.child_id" +
+            "   AND p.parent_id=%s"
+            )
+        result = db.engine.execute(q, [place.id, begin_date, end_date])
+        return result.fetchone()[0]
 
     def get_stats(self, place):
         pass
